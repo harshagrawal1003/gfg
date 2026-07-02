f14c172a19b8d5219c28ad95bfb360c340ece140
harshagrawal1003 <vindeshwariagrawal1234@gmail.com>
2026-07-02 22:29:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Subset sum divisible by k/check-subset-sum-divisible-by-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean divisibleByK(int[] arr, int k) {
+        boolean[] dp = new boolean[k];
+
+        for (int num : arr) {
+            boolean[] next = dp.clone();
+            next[num % k] = true;
+            for (int r = 0; r < k; r++) {
+                if (dp[r]) {
+                    next[(r + num) % k] = true;
+                }
+            }
+            dp = next;
+            if (dp[0]) return true;
+        }
+        return false;
+    }
+}
