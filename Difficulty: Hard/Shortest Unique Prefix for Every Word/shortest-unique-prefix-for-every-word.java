346862fdf0404ddbf951a0b4d7a5589ea4e77d5b
harshagrawal1003 <vindeshwariagrawal1234@gmail.com>
2026-07-20 14:42:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Shortest Unique Prefix for Every Word/shortest-unique-prefix-for-every-word.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    class TrieNode {
+        TrieNode[] child = new TrieNode[26];
+        int count = 0;
+    }
+
+    TrieNode root = new TrieNode();
+
+    void insert(String word) {
+        TrieNode curr = root;
+
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+
+            if (curr.child[idx] == null)
+                curr.child[idx] = new TrieNode();
+
+            curr = curr.child[idx];
+            curr.count++;
+        }
+    }
+
+    public ArrayList<String> findPrefixes(String[] arr) {
+
+        for (String word : arr)
+            insert(word);
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (String word : arr) {
+            TrieNode curr = root;
+            StringBuilder prefix = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                curr = curr.child[ch - 'a'];
+                prefix.append(ch);
+
+                if (curr.count == 1)
+                    break;
+            }
+
+            ans.add(prefix.toString());
+        }
+
+        return ans;
+    }
+}
